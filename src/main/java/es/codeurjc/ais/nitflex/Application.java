f74c3aed6941244f0ad455f7be7ebd7a591f582a
AjenjoRuben14 <114026662+AjenjoRuben14@users.noreply.github.com>
2024-05-10 21:05:49 +0200

    update 2 Application.java

--- src/main/java/es/codeurjc/ais/nitflex/Application.java
+++ src/main/java/es/codeurjc/ais/nitflex/Application.java
@@ -3,7 +3,7 @@ package es.codeurjc.ais.nitflex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication // test
+@SpringBootApplication // here we go
 public class Application {
 
 	public static void main(String[] args) {
