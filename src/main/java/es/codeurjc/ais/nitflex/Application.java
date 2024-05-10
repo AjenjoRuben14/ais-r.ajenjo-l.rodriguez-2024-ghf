c952dbbc9c2d31c73f928f5a496d2f3659724618
AjenjoRuben14 <114026662+AjenjoRuben14@users.noreply.github.com>
2024-05-10 20:25:13 +0200

    Update Application.java

--- src/main/java/es/codeurjc/ais/nitflex/Application.java
+++ src/main/java/es/codeurjc/ais/nitflex/Application.java
@@ -3,7 +3,7 @@ package es.codeurjc.ais.nitflex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // test
 public class Application {
 
 	public static void main(String[] args) {
