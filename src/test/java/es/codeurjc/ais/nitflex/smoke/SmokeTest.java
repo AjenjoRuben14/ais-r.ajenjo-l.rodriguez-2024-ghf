423cd54d4395709eeea6856ec0041cf351a58caa
AjenjoRuben14 <114026662+AjenjoRuben14@users.noreply.github.com>
2024-05-13 23:45:46 +0200

    Update SmokeTest.java

--- src/test/java/es/codeurjc/ais/nitflex/smoke/SmokeTest.java
+++ src/test/java/es/codeurjc/ais/nitflex/smoke/SmokeTest.java
@@ -1,5 +1,48 @@
 package es.codeurjc.ais.nitflex.smoke;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.Duration;
+
 public class SmokeTest {
-    
+
+    private static String host;
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        this.driver = new ChromeDriver(options);
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testWelcomeMessage() throws IOException {
+        URL url = new URL(host);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        int responseCode = connection.getResponseCode();
+        assertTrue(responseCode == HttpURLConnection.HTTP_OK,
+                "Error al obtener la respuesta del servidor. Código de respuesta: " + responseCode);
+    }
 }
