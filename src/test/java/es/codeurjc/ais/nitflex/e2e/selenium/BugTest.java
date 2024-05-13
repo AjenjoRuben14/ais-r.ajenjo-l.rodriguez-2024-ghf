b249addbe144f28a266e0848cdbca586721b72d8
AjenjoRuben14 <114026662+AjenjoRuben14@users.noreply.github.com>
2024-05-13 20:13:32 +0200

    Update BugTest.java

--- src/test/java/es/codeurjc/ais/nitflex/e2e/selenium/BugTest.java
+++ src/test/java/es/codeurjc/ais/nitflex/e2e/selenium/BugTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
